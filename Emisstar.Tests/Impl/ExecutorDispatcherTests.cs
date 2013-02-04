37812218dc9811fadea4a5704ba0b96acdf137cc
Jury Soldatenkov <yurysold@googlemail.com>
2013-02-04 15:46:43 +0400

    Implemented OrderedDispatcher (relates to #1)

---
+++ Emisstar.Tests/Impl/ExecutorDispatcherTests.cs
@@ -0,0 +1,38 @@
+﻿using Codestellation.DarkFlow.Execution;
+using Codestellation.Emisstar.Impl;
+using Codestellation.Emisstar.Testing;
+using NUnit.Framework;
+
+namespace Codestellation.Emisstar.Tests.Impl
+{
+    [TestFixture]
+    public class ExecutorDispatcherTests
+    {
+        [OrderedExecution]
+        public class MarkedMessage { }
+
+        [Test]
+        public void Can_invokes_return_true_if_message_marked()
+        {
+            var executor = new SynchronousExecutor();
+            var dispatcher = new OrderedDispatcher(executor);
+            var message = new MarkedMessage();
+            var handler = new TestHandler<MarkedMessage>();
+
+            Assert.IsTrue(dispatcher.CanInvoke(message, handler));
+        }
+
+        [Test]
+        public void Invokes_handler_using_executor()
+        {
+            var executor = new SynchronousExecutor();
+            var dispatcher = new OrderedDispatcher(executor);
+            var message = new MarkedMessage();
+            var handler = new TestHandler<MarkedMessage>();
+
+            dispatcher.Invoke(message, handler);
+
+            Assert.That(handler.WasCalledOnce);
+        }
+    }
+}
---
+++ Emisstar/Impl/OrderedDispatcher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using Codestellation.DarkFlow;
+
+namespace Codestellation.Emisstar.Impl
+{
+    public class OrderedDispatcher : RuleBasedDispatcher
+    {
+        private readonly IExecutor _orderedExecutor;
+
+        public OrderedDispatcher(IExecutor orderedExecutor): base(new OrderedRule())
+        {
+            if (orderedExecutor == null)
+            {
+                throw new ArgumentNullException("orderedExecutor");
+            }
+
+            _orderedExecutor = orderedExecutor;
+        }
+
+        private class InvokeHandlerTask<TMessage> : ITask
+        {
+            private readonly TMessage _message;
+            private readonly IHandler<TMessage> _handler;
+
+            public InvokeHandlerTask(TMessage message, IHandler<TMessage> handler)
+            {
+                Debug.Assert(message != null);
+                Debug.Assert(handler != null);
+                _message = message;
+                _handler = handler;
+            }
+
+            public void Execute()
+            {
+                _handler.Handle(_message);
+            }
+        }
+        protected override void IntervalInvoke<TMessage>(TMessage message, IHandler<TMessage> handler)
+        {
+            var task = new InvokeHandlerTask<TMessage>(message, handler);
+
+            _orderedExecutor.Execute(task);
+        }
+    }
+}
