87cbb90f55534e5c73517a4db9cbec0b4d3e644c
Jury Soldatenkov <yurysold@googlemail.com>
2013-04-01 22:59:50 +0400

    InvokeHandlerTask would be a separate class

--- Emisstar/Impl/ExecutorDispatcher.cs
+++ Emisstar/Impl/ExecutorDispatcher.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using Codestellation.DarkFlow;
 
 namespace Codestellation.Emisstar.Impl
@@ -18,24 +17,6 @@ namespace Codestellation.Emisstar.Impl
             _executor = executor;
         }
 
-        private class InvokeHandlerTask<TMessage> : ITask
-        {
-            private readonly TMessage _message;
-            private readonly IHandler<TMessage> _handler;
-
-            public InvokeHandlerTask(TMessage message, IHandler<TMessage> handler)
-            {
-                Debug.Assert(message != null);
-                Debug.Assert(handler != null);
-                _message = message;
-                _handler = handler;
-            }
-
-            public void Execute()
-            {
-                _handler.Handle(_message);
-            }
-        }
         protected override void IntervalInvoke<TMessage>(TMessage message, IHandler<TMessage> handler)
         {
             var task = new InvokeHandlerTask<TMessage>(message, handler);
