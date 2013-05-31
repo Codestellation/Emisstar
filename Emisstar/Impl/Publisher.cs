dcfbeb7d891272171ad06c9c2c3caf7897cbefdf
Jury Soldatenkov <yurysold@googlemail.com>
2013-05-31 12:16:26 +0400

    Fixed bug with ugly cacge instance at static methods

--- Emisstar/Impl/Publisher.cs
+++ Emisstar/Impl/Publisher.cs
@@ -13,11 +13,11 @@ namespace Codestellation.Emisstar.Impl
         private readonly IHandlerSource _handlerSource;
         private readonly IDispatcher[] _dispatchers;
         private readonly MethodInfo _method;
-        private static Dictionary<Type, Action<object>> _invokersCache;
+        private static Dictionary<Type, Action<Publisher, object>> _invokersCache;
 
         static Publisher()
         {
-            _invokersCache = new Dictionary<Type, Action<object>>();
+            _invokersCache = new Dictionary<Type, Action<Publisher, object>>();
         }
 
         public Publisher(IHandlerSource handlerSource, IDispatcher[] dispatchers)
@@ -61,26 +61,26 @@ namespace Codestellation.Emisstar.Impl
 
         private void Publish(object message)
         {
-            Action<object> invoker;
+            Action<Publisher, object> invoker;
             var messageType = message.GetType();
             if (!_invokersCache.TryGetValue(messageType, out invoker))
             {
                 invoker = BuildAndCacheInvoker(messageType);
             }
-            invoker(message);
+            invoker(this, message);
         }
 
-        private Action<object> BuildAndCacheInvoker(Type messageType)
+        private Action<Publisher, object> BuildAndCacheInvoker(Type messageType)
         {
-            Action<object> result = null;
-            Dictionary<Type, Action<object>> original;
-            Dictionary<Type, Action<object>> cache;
+            Action<Publisher, object> result = null;
+            Dictionary<Type, Action<Publisher, object>> original;
+            Dictionary<Type, Action<Publisher, object>> cache;
             do
             {
                 cache = _invokersCache;
                 Thread.MemoryBarrier();
                 
-                Action<object> cached;
+                Action<Publisher, object> cached;
 
                 //May be another thread filled cache already.
                 if (cache.TryGetValue(messageType, out cached))
@@ -92,7 +92,7 @@ namespace Codestellation.Emisstar.Impl
                 result = result ?? BuildExpression(messageType);
 
                 //Adds new expression to cache preserving already cached expressions. 
-                var newCache = new Dictionary<Type, Action<object>>(cache) {{messageType, result}};
+                var newCache = new Dictionary<Type, Action<Publisher, object>>(cache) { { messageType, result } };
 
                 original = Interlocked.CompareExchange(ref _invokersCache, newCache, cache);
 
@@ -100,17 +100,19 @@ namespace Codestellation.Emisstar.Impl
             return result;
         }
 
-        private Action<object> BuildExpression(Type messageType)
+        private Action<Publisher, object> BuildExpression(Type messageType)
         {
-            var parameter = Expression.Parameter(typeof (object));
+            var publisherParameter = Expression.Parameter(typeof(Publisher));
 
-            var castedArgument = Expression.Convert(parameter, messageType);
-            var instance = Expression.Constant(this);
+            var messageParameter = Expression.Parameter(typeof (object));
+
+            var castedArgument = Expression.Convert(messageParameter, messageType);
+            
             var method = _method.MakeGenericMethod(messageType);
 
-            var internalPublish = Expression.Call(instance, method, castedArgument);
+            var internalPublish = Expression.Call(publisherParameter, method, castedArgument);
 
-            var lambda = Expression.Lambda<Action<object>>(internalPublish, parameter);
+            var lambda = Expression.Lambda<Action<Publisher, object>>(internalPublish, publisherParameter, messageParameter);
             
             return lambda.Compile();
         }
@@ -132,7 +134,5 @@ namespace Codestellation.Emisstar.Impl
                 dispatcher.Invoke(message, handler);
             }
         }
-
-        
     }
 }
