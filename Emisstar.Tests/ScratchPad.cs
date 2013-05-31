00016d486bfa3c3d41eb1e3dd3862a2416aca830
Jury Soldatenkov <yurysold@googlemail.com>
2013-05-31 09:55:15 +0400

    Added scratchpad

---
+++ Emisstar.Tests/ScratchPad.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Codestellation.Emisstar.Tests
+{
+    [TestFixture]
+    public class ScratchPad
+    {
+        [Test]
+        public void ScratchTest()
+        {
+
+        }
+    }
+}
