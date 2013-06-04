0201c629c519ca340f3932138f23233f56a920d1
Jury Soldatenkov <yurysold@googlemail.com>
2013-06-04 11:16:16 +0400

    Added builder functionality to emisstar facility

--- Emisstar.CastleWindsor/Facility/EmisstarFacility.cs
+++ Emisstar.CastleWindsor/Facility/EmisstarFacility.cs
@@ -49,26 +49,6 @@ namespace Codestellation.Emisstar.CastleWindsor.Facility
                     .LifestyleSingleton());
         }
 
-        protected virtual void RegisterDispatchers()
-        {
-            _dispatcherRegistrations.Add(
-                Component
-                    .For<IDispatcher>()
-                    .ImplementedBy<SimpleDispatcher>()
-                    .LifestyleSingleton());
-            
-            Kernel.Register(_dispatcherRegistrations.ToArray());
-        }
-
-        protected virtual void RegisterPublisher()
-        {
-            Kernel.Register(
-                Component
-                    .For<IPublisher>()
-                    .ImplementedBy<Publisher>()
-                    .LifestyleSingleton());
-        }
-
         public EmisstarFacility UseDarkFlowDispatcher()
         {
             var asmBuilder = new IntegrationAssemblyBuilder();
@@ -80,6 +60,54 @@ namespace Codestellation.Emisstar.CastleWindsor.Facility
                     .LifestyleSingleton());
 
             return this;
+        }
+
+        public EmisstarFacility UseSimpleDispatcher(params IDispatchRule[] rules)
+        {
+            return RegisterRuleBaseDispatcher<SimpleDispatcher>(rules);
+        }
+
+        public EmisstarFacility UseSynchronizationContextDispatcher(params IDispatchRule[] rules)
+        {
+            return RegisterRuleBaseDispatcher<SynchronizationContextDispatcher>(rules);
+        }
+
+        private EmisstarFacility RegisterRuleBaseDispatcher<TDispatcher>(params IDispatchRule[] rules)
+            where TDispatcher : RuleBasedDispatcher
+        {
+            var componentRegistration =
+                Component
+                    .For<IDispatcher>()
+                    .ImplementedBy<TDispatcher>()
+                    .LifestyleSingleton();
+
+            if (rules != null && rules.Length > 0)
+            {
+                componentRegistration
+                    .DependsOn(Dependency.OnValue<IDispatchRule[]>(rules));
+            }
+
+            _dispatcherRegistrations.Add(componentRegistration);
+            return this;
+        }
+
+        private void RegisterPublisher()
+        {
+            Kernel.Register(
+                Component
+                    .For<IPublisher>()
+                    .ImplementedBy<Publisher>()
+                    .LifestyleSingleton());
+        }
+
+        private void RegisterDispatchers()
+        {
+            if (_dispatcherRegistrations.Count == 0)
+            {
+                UseSimpleDispatcher();
+            }
+
+            Kernel.Register(_dispatcherRegistrations.ToArray());
         }
     }
 }
