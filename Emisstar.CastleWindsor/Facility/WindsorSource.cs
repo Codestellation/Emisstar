26b5b6681f9b1691f63d5a1eeefb7f7d0938e342
Jury Soldatenkov <yurysold@gmail.com>
2013-07-10 13:04:16 +0400

    Optimized windsor source

--- Emisstar.CastleWindsor/Facility/WindsorSource.cs
+++ Emisstar.CastleWindsor/Facility/WindsorSource.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Castle.MicroKernel;
 using Codestellation.Emisstar.Impl;
 
@@ -9,6 +10,7 @@ namespace Codestellation.Emisstar.CastleWindsor.Facility
     public class WindsorSource : IHandlerSource
     {
         private readonly IKernel _kernel;
+        private Dictionary<Type, Type> _cache;
 
         public WindsorSource(IKernel kernel)
         {
@@ -16,15 +18,46 @@ namespace Codestellation.Emisstar.CastleWindsor.Facility
             {
                 throw new ArgumentNullException("kernel");
             }
-
+            _cache = new Dictionary<Type, Type>();
             _kernel = kernel;
         }
 
         public IEnumerable<object> ResolveHandlersFor(Type messageType)
         {
-            var handlerType = typeof (IHandler<>).MakeGenericType(messageType);
+            Type handlerType;
+            if (!_cache.TryGetValue(messageType, out handlerType))
+            {
+                handlerType = BuildAndCache(messageType);
+            }
 
             return _kernel.ResolveAll(handlerType).Cast<object>();
+        }
+
+        //TODO: Move it to submodule or Common project
+        private Type BuildAndCache(Type messageType)
+        {
+            Type result;
+            Dictionary<Type,Type> cacheBeforeCAS;
+            Dictionary<Type, Type> cacheAfterCAS;
+
+            do
+            {
+                cacheBeforeCAS = _cache;
+                Thread.MemoryBarrier();
+
+                if (cacheBeforeCAS.TryGetValue(messageType, out result))
+                {
+                    return result;
+                }
+                result = typeof(IHandler<>).MakeGenericType(messageType);
+
+                var newCache = new Dictionary<Type, Type>(cacheBeforeCAS) { { messageType, result } };
+
+                cacheAfterCAS = Interlocked.CompareExchange(ref _cache, newCache, cacheBeforeCAS);
+
+            } while (cacheBeforeCAS != cacheAfterCAS);
+
+            return result;
         }
     }
 }
