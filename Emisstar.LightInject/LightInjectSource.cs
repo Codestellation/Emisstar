85c05ae5538059fd455f3643b3314e41338f8d81
Jury Soldatenkov <yurysold@gmail.com>
2015-04-10 16:51:00 +0300

    Implemented handler source for light inject

---
+++ Emisstar.LightInject/LightInjectSource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Codestellation.Emisstar.Impl;
+using LightInject;
+
+namespace Emisstar.LightInject
+{
+    public class LightInjectSource : IHandlerSource
+    {
+        private readonly IServiceFactory _container;
+
+        public LightInjectSource(IServiceFactory container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+
+            _container = container;
+        }
+
+        public IEnumerable<object> ResolveHandlersFor(Type messageType)
+        {
+            var handlerType = messageType.GetHandlerType();
+            return _container.GetAllInstances(handlerType);
+        }
+    }
+}
---
+++ Emisstar.Tests/LightInject/LightInjectorHandlerSourceTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Codestellation.Emisstar.Testing;
+using Codestellation.Emisstar.Tests.Windsor;
+using Emisstar.LightInject;
+using LightInject;
+using NUnit.Framework;
+
+namespace Codestellation.Emisstar.Tests.LightInject
+{
+    [TestFixture]
+    public class LightInjectorHandlerSourceTests
+    {
+        [Test]
+        public void Resolve_returns_instances_from_kernel()
+        {
+            var container = new ServiceContainer();
+            container.Register<IHandler<Message>, TestHandler<Message>>(new PerContainerLifetime());
+            container.Register<IHandler<Message>, TestHandler<Message>>("Other");
+
+            var source = new LightInjectSource(container);
+
+            var handlers = source.ResolveHandlersFor(typeof(Message));
+
+            Assert.That(handlers.Count(), Is.EqualTo(2));
+        } 
+    }
+}
